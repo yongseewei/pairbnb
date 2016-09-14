b0721af93867392e90091ebc087a9590fc441634
sylviakyc <sylviakyc@sylviakycs-MacBook-Air.local>
2016-09-14 18:04:57 +0800

    Day 1 - User Authentication

---
+++ db/migrate/20160914082841_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+  	add_column :users, :firstname, :string
+  	add_column :users, :lastname, :string
+  	add_column :users, :birthday, :date
+  end
+end
